102d0a55dc85b286a2ca20c1e0d23c47aaaacc89
Clayton Winterbotham <cwinterbotham@gmail.com>
2018-05-26 20:30:02 -0700

    first commit

--- AddressBook/AddressBook.Designer.cs
+++ AddressBook/AddressBook.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\repos\Address Book\AddressBook\AddressBook\AddressBook.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\cwinterbotham\source\repos\Address Book\AddressBook\AddressBook\AddressBook.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
